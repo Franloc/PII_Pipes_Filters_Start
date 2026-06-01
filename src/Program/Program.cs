c63b3eac8a457acd34b503e2a34942dc3252870a
Franloc <francesco.locatelli@correo.ucu.edu.uy>
2026-06-01 17:22:31 -0300

    Trabajo

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
+using Ucu.Poo.Twitter;
 
 namespace CompAndDel
 {
@@ -9,14 +10,23 @@ namespace CompAndDel
         static void Main(string[] args)
         {
             PipeNull pipeNull = new PipeNull();
-            PipeSerial pipeSerial2 = new PipeSerial(new FilterNegative(), pipeNull);
-            PipeSerial pipeSerial1 = new PipeSerial(new FilterGreyscale(), pipeSerial2);
+            PipeSerial pipeSerial4 = new PipeSerial(new FilterTwitter("resultado.jpg"), pipeNull); // Publica en twitter
+            PipeSerial pipeSave = new PipeSerial(new FilterSave("resultado.jpg"), pipeSerial4); // Guarda la imagen de Luke con el filtro aplicado previamente
+            PipeSerial pipeSerial3True = new PipeSerial(new FilterGreyscale(), pipeSave); // Si reconoció que tiene cara, le aplica un filtro de grises y lo manda a pipeSave
+            PipeSerial pipeSerial3False = new PipeSerial(new FilterNegative(), pipeSave); // Si reconoció que no tiene cara, le aplica un filtro negativo y lo manda a pipeSave
+            PipeIf pipeIf = new PipeIf(pipeSerial3False, pipeSerial3True, new FilterCognitive("luke.jpg")); // Aplico el filtro de reconocimiento facial a luke.jpg
+
+            PipeNull pipeNullBeer = new PipeNull();
+            PipeSerial pipeSerialBeerfalse = new PipeSerial(new FilterTwitter("Beer1.jpg"), pipeNullBeer); // Si no reconoció cara, la publica en twitter
+            PipeSerial pipeSerialBeertrue = new PipeSerial(new FilterNegative(), pipeNullBeer); // Si reconoció una cara, le aplica el filtro de negativo
+            PipeIf PipeIfBeer = new PipeIf(pipeSerialBeerfalse, pipeSerialBeertrue, new FilterCognitive("Beer1.jpg")); // Aplica el filtro de reconocimiento a la cerveza
+            PipeSerial PipeSerialBeer1 = new PipeSerial(new FilterSave("Beer1.jpg"), PipeIfBeer); // Guarda la imagen como Beer1 y la manda a la pipe condicional
 
             PictureProvider provider = new PictureProvider();
-            IPicture picture = provider.GetPicture(@"luke.jpg");
-
-            IPicture result = pipeSerial1.Send(picture);
-            provider.SavePicture(result, @"luke1.jpg");
+            IPicture picture_luke = provider.GetPicture("luke.jpg");
+            IPicture picture_beer = provider.GetPicture("beer.jpg");
+            pipeIf.Send(picture_luke);
+            PipeSerialBeer1.Send(picture_beer);
         }
     }
 }
